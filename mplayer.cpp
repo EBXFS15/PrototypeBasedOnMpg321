bdbe58c041a18219c0a87eeea32b3522111a4938
Cedric Ocaña <github@ocana.be>
2015-05-27 16:15:04 +0200

    Minnor refactoring

--- mplayer.cpp
+++ mplayer.cpp
@@ -13,8 +13,8 @@ mplayer::mplayer(QObject *parent) : QObject(parent)
 
 mplayer::~mplayer()
 {
-    player->write("stop\n");
-    player->write("quit 0\n");
+    sendCommandToPlayer("stop");
+    sendCommandToPlayer("quit");
     player->closeWriteChannel();
     player->waitForFinished();
     delete player;
@@ -33,14 +33,14 @@ void mplayer::player_update()
         while ((pos = rx.indexIn(playBackInfo, pos)) != -1) {
             list << rx.cap(1);
             pos += rx.matchedLength();
-        }
+        }        
         if (list.count() == 2){
             float currentPostion = list[0].toFloat();
             float duration = list[1].toFloat();
-            if ((duration - currentPostion) < 0.3){
+            if ((duration - currentPostion) < 0.3){                
                 //stop();
                 //emit playbackPosition(100);
-                //emit playbackEnded();
+                //emit playbackEnded();                
             }
             else{
                 int position = (currentPostion / duration)*100;
@@ -51,52 +51,61 @@ void mplayer::player_update()
     emit statusChanged(msg);
 }
 
+/*! Adds a line-feed to the QByteArray in order to trigger the player to start interpretation of cmd*/
+void mplayer::sendCommandToPlayer(QByteArray cmd){
+    cmd.append("\n");
+    player->write(cmd);
+}
 
+/*! defines the file to play when the play function is called*/
 void mplayer::loadFile(QString path){
     nextFile = path;
 }
 
-void mplayer::play(){
-    /**! unpause if playback is paused and play is called */
+void mplayer::play(){    
+    /*! Continues playback if play is pressed while the player is paused*/
     if(paused == true){
         pause();
         return;
-    }
-    player->readAll();
+    }    
+
     currentFile = nextFile;
     QByteArray cmd = "loadfile ";
+
+    /*! Add double quotes to avoid missinterpretation of path by the player*/
     QString path = currentFile;
     path.prepend("\"");
     path.append("\"");
-
     cmd.append(path);
-    cmd.append("\n");
-    player->write(cmd);
+
+    sendCommandToPlayer(cmd);
+
+    /*! Continues playback if play is pressed while the player is paused*/
     paused = false;
+
     return;
 
 }
 
 void mplayer::stop(){
-    player->write("stop\n");
+    sendCommandToPlayer("stop");
     currentFile = "";
 }
 
 void mplayer::ff(int frames){
     QByteArray cmd = "seek +";
     cmd.append(QString::number(frames/100));
-    cmd.append("\n");
-    player->write(cmd);
+    sendCommandToPlayer(cmd);
 }
 
 void mplayer::rw(int frames){
     QByteArray cmd = "seek -";
     cmd.append(QString::number(frames/100));
-    cmd.append("\n");
-    player->write(cmd);
+    sendCommandToPlayer(cmd);
 }
 
 void mplayer::pause(){
-    player->write("pause\n");
+    sendCommandToPlayer("pause");
+    /*! Continues playback if play is pressed while the player is paused*/
     paused = !paused;
 }
